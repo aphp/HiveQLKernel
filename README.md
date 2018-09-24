daa9b02490f1691e2743a4b2670165ee3b1b4ba9
j.du <julien.dubiel@aphp.fr>
2018-09-24 15:26:07 +0200

    Working kernel!

--- README.md
+++ README.md
@@ -1,7 +1,40 @@
 # HiveQL Kernel
 
+If you are going to connect using kerberos:
+
+```
+sudo apt-get install python3-dev libsasl2-dev libsasl2-2 libsasl2-modules-gssapi-mit
+```
+
+
+To install the kernel:
 
 ```
 pip install .
 jupyter hiveql install --user
 ```
+
+
+How to use in Notebooks:
+
+
+First create your connection, inside a Notebook cell configure the connection:
+
+```
+$$ url=hive://<kerberos-username>@<hive-host>:<hive-port>/<db-name>
+$$ connect_args={"auth": "KERBEROS","kerberos_service_name": "hive"}
+$$ pool_size=5
+$$ max_overflow=10
+```
+
+These args are passed to sqlalchemy, who registered pyHive as the 'hive' SQL back-end.
+See [github.com/dropbox/PyHive](https://github.com/dropbox/PyHive/#sqlalchemy).
+
+
+You can now type HiveQL inside the next Notebook cells:
+
+```
+SHOW TABLES
+```
+
+Have fun!
--- hiveql/kernel.py
+++ hiveql/kernel.py
@@ -1,41 +1,40 @@
+import json
 import logging
-import string
+import traceback
 
 from ipykernel.kernelbase import Kernel
+from sqlalchemy.exc import OperationalError
+
+from .constants import __version__, KERNEL_NAME
+
+from sqlalchemy import *
+import pandas as pd
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-from .constants import __version__, KERNEL_NAME
 
-from random import randint, choice
-
-
-def gen_fake_array():
-    nb_lines = randint(0, 1000)
-    nb_cols = randint(0, 150)
-    return [''.join(choice(string.ascii_lowercase) for __ in range(randint(2, 15))) for _ in range(nb_cols)], [
-        [randint(-1500, + 1500) * randint(0, 1) / randint(1, 9) for __ in range(nb_cols)] for _ in range(nb_lines)
-    ]
+class KernelSyntaxError(Exception):
+    pass
 
 
-def array_to_html(titles, array, limit=20):
-    result = "<h2>Count = {}</h2><table class='table table-striped'>".format(len(array))
-    result += "<thead><tr>"
-    for title in titles:
-        result += "<th>{}</th>".format(title)
-    result += "</tr></thead><tbody>".format(titles)
-    for i, line in enumerate(array):
-        if i > limit:
-            result += '<tr><td>...</td></tr>'
-            break
-        result += "<tr>"
-        for e in line:
-            result += "<td>{}</td>".format(str(e))
-        result += "</tr>"
-    result += "</tbody></table>"
-    logger.log(logging.INFO, result)
-    return result
+error_con_not_created = """Connection not initialized!
+Please specify your pyHive configuration like this (if you want to update the current connection, just type it again with another configuration):
+
+-------------
+$$ url=hive://<kerberos-username>@<hive-host>:<hive-port>/<db-name>
+$$ connect_args={"auth": "KERBEROS","kerberos_service_name": "hive"}
+$$ pool_size=5
+$$ max_overflow=10
+
+YOUR SQL REQUEST HERE IF ANY
+-------------
+"""
+
+
+class ConnectionNotCreated(Exception):
+    def __init__(self):
+        Exception.__init__(self, error_con_not_created)
 
 
 class HiveQLKernel(Kernel):
@@ -45,36 +44,104 @@ class HiveQLKernel(Kernel):
     language_info = {
         'name': 'hive',
         'codemirror_mode': 'python',
-        'pygments_lexer': 'psql',
+        'pygments_lexer': 'sql',
         'mimetype': 'text/plain',
         'file_extension': '.hiveql',
     }
+    last_conn = None
+
+    def send_exception(self, e):
+        tb = traceback.format_exc()
+        return self.send_error(str(e) + '\n' + tb)
+
+    def send_error(self, contents):
+        self.send_response(self.iopub_socket, 'stream', {
+            'name': 'stderr',
+            'text': str(contents)
+        })
+        return {
+            'status': 'error',
+            'execution_count': self.execution_count,
+            'payload': [],
+            'user_expressions': {}
+        }
+
+    def send_info(self, contents):
+        self.send_response(self.iopub_socket, 'stream', {
+            'name': 'stdout',
+            'text': str(contents)
+        })
+
+    def create_conn(self, url, **kwargs):
+        self.send_info("create_engine('" + url + "', " + ', '.join(
+            [str(k) + '=' + (str(v) if type(v) == str else json.dumps(v)) for k, v in kwargs.items()]) + ")\n")
+        self.last_conn = create_engine(url, **kwargs)
+        self.last_conn.connect()
+        self.send_info("Connection established to database!\n")
+
+    def parse_code(self, code):
+        req = code.strip()
+
+        headers = {}
+        sql_req = ""
+        beginning = True
+        for l in req.split('\n'):
+            l = l.strip()
+            if l.startswith("$$"):
+                if beginning:
+                    k, v = l.replace("$", "").split("=")
+                    k, v = k.strip(), v.strip()
+                    if v.startswith('{'):
+                        v = json.loads(v)
+                    else:
+                        try:
+                            v = int(v)
+                        except ValueError:
+                            pass
+                    headers[k] = v
+                else:
+                    raise KernelSyntaxError("Headers starting with %% must be at the beginning of your request.")
+            else:
+                beginning = False
+                sql_req += ' ' + l
+
+        sql_req = sql_req.strip()
+        if sql_req.endswith(';'):
+            sql_req = sql_req[:-1]
+
+        return headers, sql_req
 
     def do_execute(self, code, silent, store_history=True, user_expressions=None, allow_stdin=False):
-        if not code.strip():
-            return {
-                'status': 'ok',
-                'execution_count': self.execution_count,
-                'payload': [],
-                'user_expressions': {}
-            }
+        try:
+            headers, sql_req = self.parse_code(code)
+
+            if 'url' in headers:
+                self.create_conn(**headers)
+
+            if self.last_conn is None:
+                raise ConnectionNotCreated()
+
+            # If code empty
+            if not sql_req:
+                return {
+                    'status': 'ok',
+                    'execution_count': self.execution_count,
+                    'payload': [],
+                    'user_expressions': {}
+                }
+
+            html = pd.read_sql(sql_req, self.last_conn).to_html()
+        except OperationalError as oe:
+            return self.send_error(oe)
+        except Exception as e:
+            return self.send_exception(e)
 
         # msg_types = https://jupyter-client.readthedocs.io/en/latest/messaging.html?highlight=stream#messages-on-the-iopub-pub-sub-channel
         self.send_response(self.iopub_socket, 'execute_result', {
             "execution_count": self.execution_count,
             'data': {
-                # "text/plain" : ["multiline text data"],
-                "text/html": array_to_html(*gen_fake_array()),
-                # "application/json": {
-                # 	"json": "data"
-                # }
+                "text/html": html,
             },
-            "metadata": {
-                "image/png": {
-                    "width": 640,
-                    "height": 480,
-                },
-            }
         })
 
         return {
